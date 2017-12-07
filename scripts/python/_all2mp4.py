99ceda6b5eb258f65fd15ee99526051d5be6fbaf
Bryn Mosher <sites@brynmosher.com>
2017-12-06 23:41:42 -0800

    add cli args for mkv recipe, force replace, and verbosity; minor style cleanup

--- scripts/python/_all2mp4.py
+++ scripts/python/_all2mp4.py
@@ -2,17 +2,34 @@ import os
 import re
 import sys
 import traceback
+import argparse
 from subprocess import call, Popen, PIPE, STDOUT
 
-srcDir=os.path.dirname(os.path.realpath(__file__))
-destDir=os.path.join(srcDir, 'mp4')
+srcDir = os.path.dirname(os.path.realpath(__file__))
+destDir = os.path.join(srcDir, 'mp4')
+mkvRecipe = '-c:a libmp3lame -q:a 2'
 
 rgxExtNonMp4Video = re.compile("\.(mkv|webm|avi|mov)$", re.I | re.M)
 rgxCtrLf = re.compile("", re.I)
 
-ffmpegTemplate='ffmpeg -i "{inPath}" "{outPath}"'
-#ffmpegTemplate='ffmpeg -i "{inPath}" -loglevel debug -c:a libmp3lame -q:a 2 "{outPath}"'
-#ffmpegTemplate='ffmpeg -i "{inPath}" -loglevel debug "{outPath}"'
+argParser = argparse.ArgumentParser()
+argParser.add_argument('-v', '--verbose', help='debug/verbose ffmpeg', action='store_true')
+argParser.add_argument('-k', '--mkv', help='use mkv ffmpeg recipe', action='store_true')
+argParser.add_argument('-f', '--force', help='overwrite existing mp4 files found', action='store_true')
+args = argParser.parse_args()
+
+ffmpegTemplate='ffmpeg -i "{inPath}" '
+
+# Sample command:
+#    ffmpegTemplate='ffmpeg -i "{inPath}" -loglevel debug -c:a libmp3lame -q:a 2 "{outPath}"'
+
+# Parse arguments for ffmpeg bits...
+if args.verbose:
+	ffmpegTemplate += '-loglevel debug '
+if args.mkv:
+	ffmpegTemplate += mkvRecipe+' '
+# Append the output file to the final ffmpeg command
+ffmpegTemplate += '"{outPath}"'
 
 def makeOutputDir(dirToMake):
 	if not os.path.isdir(dirToMake):
@@ -24,26 +41,33 @@ def findMkvs():
 
 def processFolder():
 	print('Searching "{folder}".'.format(folder=srcDir))
-	mkvsFound=findMkvs()
+	mkvsFound = findMkvs()
 	print('Found {n} files.'.format(n=len(mkvsFound)))
 
 	makeOutputDir(destDir)
 	for i, name in enumerate(mkvsFound):
 		if rgxExtNonMp4Video.search(name):
 			print('\n#\n# File #{iteration} is non-pm4 video:\n#    "{name}"\n#'.format(iteration=i, name=name))
-			destFqp=getDestFqp(name)
+			destFqp = getDestFqp(name)
+			weAreGo = True
 			if os.path.exists(destFqp):
-				print('# Destination file already exists:\n#    {destFqp}\n#'.format(destFqp=destFqp))
-			else:
-				inFqp=os.path.join(srcDir, name)
-				ffmpegCommand=composeMkvToMp4Command(inFqp, destFqp)
+				if args.force:
+					print('# WARN Removing existing MP4:\n#    {destFqp}\n#'.format(destFqp=destFqp))
+					os.remove(destFqp)
+				else:
+					weAreGo = False
+					print('# Destination file already exists:\n#    {destFqp}\n#'.format(destFqp=destFqp))
+
+			if weAreGo:
+				inFqp = os.path.join(srcDir, name)
+				ffmpegCommand = composeMkvToMp4Command(inFqp, destFqp)
 				print('# Running command:\n#    {command}\n#'.format(command=ffmpegCommand))
 				if not runCommand(ffmpegCommand):
 					print('#\n# WARNING!\n# ffmpeg encountered an error. Halting. Output file may be incomplete.\n#')
 					break
 
 def getDestFqp(fileName):
-	extensionless=re.sub(rgxExtNonMp4Video, '', fileName)
+	extensionless = re.sub(rgxExtNonMp4Video, '', fileName)
 	return os.path.join(destDir, extensionless+'.mp4')
 			
 def composeMkvToMp4Command(fqpIn, fqpOut):
